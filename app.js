a17ef7d6f7b1554dd7ab29b24356402679b9a691
hemzi <aaron.stacey@proton.me>
2022-11-02 20:18:46 -0500

    password endpoint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 
 const app = express();
@@ -8,6 +9,10 @@ app.get("/", (req, res) => {
   res.status(200).send({ data: "Johnny 5 is alive!" });
 });
 
+app.get("/password", (req, res) => {
+  res.status(200).send({ data: genPassword() });
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.error(err);
@@ -15,3 +20,13 @@ app.listen(port, (err) => {
     console.log(`Express listening on ${port}`);
   }
 });
+
+function genPassword() {
+  const chars =
+    "abcdefghijkmnopqrstuvwxzyABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
+  let pass = "";
+  for (i = 0; i < 20; i++) {
+    pass += chars[Math.floor(chars.length * Math.random())];
+  }
+  return pass;
+}
